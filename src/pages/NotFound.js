30b6769054c3af0dec4342596963512cbceb95d3
Asmaa100 <asmaaebrahim11298@gmail.com>
2022-04-18 13:26:44 +0200

    Create React App

---
+++ src/pages/NotFound.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function NotFound() {
+  return (
+    <div className='container mt-5'>
+        <h2 className='text-center'>Not Found :( </h2>
+    </div>
+  )
+}
+
+export default NotFound
---
+++ src/pages/favourites.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function favourites(props) {
+  console.log(props)
+  return (
+    <>
+    
+    Welcome To Favorites
+    </>
+  )
+}
+
+export default favourites
+
